48193ce84524f488814b1d15c46ff579125a55c1
Luffy <luffy20041225@gmail.com>
2024-08-29 17:28:03 +0800

    Font Problem Fixed

--- AIModel.py
+++ AIModel.py
@@ -174,6 +174,8 @@ class AI_Model:
             model_full_name = f'{model_name}_Epoch_{ self._Epoch}.h5'
             model_full_name = os.path.join(models_path, model_full_name)
 
+            print(f"Model {model_name}保存到: {model_full_name}")
+
             self.Model.save(model_full_name)
         else:
             print("Model is None.")
@@ -274,20 +276,11 @@ class AI_Model:
             _font_name = 'taipei_sans_tc_beta.ttf'
             font_path = os.path.join("font",_font_name)
 
-            passflag = self._check_and_download_font(font_path) # 檢查 font 存不存在, 不存在不能執行
 
-            if(not passflag):
+            if( not os.path.exists(font_path)):
+
+                print(f"找不到 font {font_path}")
                 return
-
-            # if( not os.path.exists(font_path)):
-
-            #     print(f"找不到 font {font_path}")
-            #     print(f"如要使用font 請輸入:")
-            #     print('wget -O taipei_sans_tc_beta.ttf "https://drive.google.com/uc?id=1eGAsTN1HBpJAkeVM57_C7ccp7hbgSz3_&export=download"')
-
-            #     print("嘗試自動下載...")
-            #     self._download_font()
-            #     return
             
             matplotlib.font_manager.fontManager.addfont(font_path)
             matplotlib.rc('font', family='Taipei Sans TC Beta')
@@ -347,43 +340,45 @@ class AI_Model:
         return valid
     
 
-    def _download_font(self): # 不要使用 (請使用_check_and_download_font)
-        # 確保 font 資料夾存在
-        os.makedirs('font', exist_ok=True)
+    def _download_font(self): # (ABANDOM)不要使用 (請使用_check_and_download_font)
+        pass
+        # # 確保 font 資料夾存在
+        # os.makedirs('font', exist_ok=True)
 
-        # 下載字型檔案
-        url = "https://drive.google.com/uc?id=1eGAsTN1HBpJAkeVM57_C7ccp7hbgSz3_&export=download"
-        output_file = 'font/taipei_sans_tc_beta.ttf'
+        # # 下載字型檔案
+        # url = "https://drive.google.com/uc?id=1eGAsTN1HBpJAkeVM57_C7ccp7hbgSz3_&export=download"
+        # output_file = 'font/taipei_sans_tc_beta.ttf'
 
-        # 使用 wget 下載檔案
-        try:
-            subprocess.run(['wget', '-O', output_file, url], check=True)
-            print(f'File downloaded successfully and saved to {output_file}')
-        except subprocess.CalledProcessError as e:
-            print(f'Error occurred while downloading the file: {e}')
+        # # 使用 wget 下載檔案
+        # try:
+        #     subprocess.run(['wget', '-O', output_file, url], check=True)
+        #     print(f'File downloaded successfully and saved to {output_file}')
+        # except subprocess.CalledProcessError as e:
+        #     print(f'Error occurred while downloading the file: {e}')
 
-    def _check_and_download_font(self, check_font_path):
-        passflag = False
+    def _check_and_download_font(self, check_font_path):# (ABANDOM)
+        pass
+        # passflag = False
 
-        if not os.path.exists(check_font_path):
-            print(f"找不到 font {check_font_path}")
-            print("嘗試自動下載...")
+        # if not os.path.exists(check_font_path):
+        #     print(f"找不到 font {check_font_path}")
+        #     print("嘗試自動下載...")
 
-            # 嘗試下載字型檔案
-            self._download_font()
+        #     # 嘗試下載字型檔案
+        #     self._download_font()
             
-            # 再次檢查字型檔案是否存在
-            if not os.path.exists(check_font_path):
-                print(f"字型檔案仍然不存在: {check_font_path}")
-                print("請手動下載字型檔案並將其放置在 'font' 資料夾中。")
-            else:
-                print(f"字型檔案下載成功: {check_font_path}")
-                passflag = True
-        else:
-            print(f"字型檔案已存在: {check_font_path}")
-            passflag = True
+        #     # 再次檢查字型檔案是否存在
+        #     if not os.path.exists(check_font_path):
+        #         print(f"字型檔案仍然不存在: {check_font_path}")
+        #         print("請手動下載字型檔案並將其放置在 'font' 資料夾中。")
+        #     else:
+        #         print(f"字型檔案下載成功: {check_font_path}")
+        #         passflag = True
+        # else:
+        #     print(f"字型檔案已存在: {check_font_path}")
+        #     passflag = True
 
-        return passflag
+        # return passflag
 
     #endregion
 
@@ -418,6 +413,7 @@ if __name__ == '__main__':
         elif(input_str == "4"):
             Model.EvaluatePerformance()
         elif(input_str == "5"):
+            save_model_name = input("輸入 新Model名稱: ")
             Model.Save_Model()
         else:
             break
